1adf07ab18271e643decd3772b237d2d7991926d
sohamdixit <sohamddixit@gmail.com>
2024-12-08 11:27:21 +0530

    updated client id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-                                              
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 import { Web3Auth } from "@web3auth/modal";
 import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { useNavigate,BrowserRouter,Route,Routes } from 'react-router-dom';
+import { useNavigate, BrowserRouter, Route, Routes } from "react-router-dom";
 import RPC from "./web3RPC";
-import walletimg from "./img1.png"
-import Login from "./components/Login"
-import Upiform from './components/Upiform';
-import PrivateComponent from './components/PrivateComponent';
-import Scanner from './components/Scanner';
-import Header from './components/Header'
-import TransakWidget from './components/TransakWidget';
-import UniswapSubgraph from './components/UniswapSubgraph';
-import MicroInvestment from './components/MicroInvestment';
-import { OktoProvider, BuildType } from 'okto-sdk-react';
+import walletimg from "./img1.png";
+import Login from "./components/Login";
+import Upiform from "./components/Upiform";
+import PrivateComponent from "./components/PrivateComponent";
+import Scanner from "./components/Scanner";
+import Header from "./components/Header";
+import TransakWidget from "./components/TransakWidget";
+import UniswapSubgraph from "./components/UniswapSubgraph";
+import MicroInvestment from "./components/MicroInvestment";
+import { OktoProvider, BuildType } from "okto-sdk-react";
 
 function App() {
   const [web3auth, setWeb3auth] = useState(null);
@@ -26,10 +25,8 @@ function App() {
   const [UpiID, setUpiID] = useState("");
   const [loginwindow, setLoginwindow] = useState(true);
 
-
-
   const clientId =
-    "BMkKHE4n2KgzLWFXDmpCVIpWMggQ8Pe8_4pRkbm9aNafKnn0WRlb1zoy6JlOh2nN2Aw54jIAbFbsAUut3tuJr8w";
+    "BMkIcnCDXsPdrCPRI87Aleozma75z4EGhrxDzjD9dD5E9GsRekIVd4OMXN5Tiv1A4Wa4bs8DR651gyp_F1WA8Hs";
   const getUserInfo = async () => {
     if (!web3auth) {
       console.log("web3auth not initialized yet");
@@ -50,27 +47,27 @@ function App() {
     console.log(address);
     getUserInfo();
   };
-  
+
   return (
-
-    <OktoProvider apiKey={"d9e324c8-728d-4d8a-a837-cd8c613f273e"} buildType={BuildType.SANDBOX}>
+    <OktoProvider
+      apiKey={"d9e324c8-728d-4d8a-a837-cd8c613f273e"}
+      buildType={BuildType.SANDBOX}
+    >
       <div className="App">
-      
-      <BrowserRouter>
-      <Routes>
-      <Route element={<PrivateComponent/>}>
-        <Route path='/upiform' element={<Upiform/>}/>
-      </Route>
-      
-      <Route path='/' element={<Header/>}/>
-      <Route path='/scanner' element={<Scanner/>}/>
-      <Route path='/transak' element={<TransakWidget/>}/>
-      <Route path='/uniswap' element={<UniswapSubgraph/>}/>
-      <Route path='/microinvestments' element={<MicroInvestment/>}/>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PrivateComponent />}>
+              <Route path="/upiform" element={<Upiform />} />
+            </Route>
 
-      </Routes>
-      </BrowserRouter>
-    </div>
+            <Route path="/" element={<Header />} />
+            <Route path="/scanner" element={<Scanner />} />
+            <Route path="/transak" element={<TransakWidget />} />
+            <Route path="/uniswap" element={<UniswapSubgraph />} />
+            <Route path="/microinvestments" element={<MicroInvestment />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </OktoProvider>
   );
 }
